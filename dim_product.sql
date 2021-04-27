150889ea413d8e75a6ef981cce57427238494bb9
huy.truong <huy.truong@tiki.vn>
2021-04-27 18:38:21 +0700

    update cate pk

--- dim_product.sql
+++ dim_product.sql
@@ -1,4 +1,7 @@
 CREATE OR REPLACE TABLE dwh.dim_product AS 
+
+WITH data AS (
+
 SELECT
 sku
 , product_name
@@ -11,8 +14,33 @@ sku
 , SPLIT(REGEXP_REPLACE(cat_tree, r'[^\d]+',','),',')[SAFE_OFFSET(0)] cat1_id
 , SPLIT(REGEXP_REPLACE(cat_tree, r'[^\d]+',','),',')[SAFE_OFFSET(1)] cat2_id
 , SPLIT(REGEXP_REPLACE(cat_tree, r'[^\d]+',','),',')[SAFE_OFFSET(2)] cat3_id
+, IF(cat_tree LIKE '600000-Phụ kiện%', 
+        CONCAT(SUBSTR(SPLIT(REGEXP_REPLACE(cat_tree, r'[^\d]+',','),',')[SAFE_OFFSET(2)], -3,1), '00000')
+        , NULL) cat1_pk_id
 , IF(SPLIT(REGEXP_REPLACE(cat_tree, r'[^\d]+',','),',')[SAFE_OFFSET(0)] = '600000',1,0) is_pk
 , IF(SUBSTR(sku,0,2) = 'BH', 1, 0) is_bh
 , IF(product_name LIKE '%iao hàng%', 1, 0) is_delivery
+, IF(SPLIT(REGEXP_REPLACE(cat_tree, r'[^\d]+',','),',')[SAFE_OFFSET(0)] = '800000',1,0) is_amthanh
+
 FROM dwh.product
 WHERE 1 = 1
+)
+
+SELECT 
+d.sku
+, d.product_name
+, d.brand
+, d.type
+, d.cat_tree
+, d.cat1
+, d.cat2
+, d.cat3
+, d.cat1_id
+, d.cat2_id
+, d.cat3_id
+, CASE WHEN cat1_pk_id < '100000' OR cat1_pk_id >= '600000' THEN NULL ELSE cat1_pk_id END cat1_pk_id
+, d.is_pk
+, d.is_bh
+, d.is_delivery
+, d.is_amthanh
+FROM data d
--- net_sale_by_invoice_item.sql
+++ net_sale_by_invoice_item.sql
@@ -1,6 +1,7 @@
 CREATE OR REPLACE TABLE dwh.net_sale_by_invoice_item AS
 
-WITH final AS (
+WITH 
+return_detail AS (
 SELECT 
   i.branch
 , i.transaction_id
@@ -30,6 +31,7 @@ SELECT
 , dp.cat3
 , dp.brand
 , hd.department
+, dp.cat1_pk_id
 FROM dwh.invoice_detail i
 LEFT JOIN dwh.return_detail rd ON i.transaction_id = rd.transaction_id
                              AND i.sku = rd.sku
@@ -40,12 +42,35 @@ LEFT JOIN dwh.hr_department hd ON IFNULL(r.buy_staff_name,i.staff_name) = hd.sta
                               AND IFNULL(DATE(r.buy_date),DATE(i.created_at)) >= hd.from_date
                               AND IFNULL(DATE(r.buy_date),DATE(i.created_at)) < hd.to_date
 )
+,
 
+pk_raw AS (
 SELECT 
-    f.* EXCEPT(is_pk, is_delivery)
-    ,IF(is_pk = 0, 0, IF(amount_edit > 0,1,0)) is_pk
-    ,IF(SUM(IF(is_pk  = 0, amount_edit, 0)) OVER (PARTITION BY transaction_id_relink) >3000000,1,0) is_amount_edit_exc_pk_over_3mil
+    r.* EXCEPT(is_pk, is_delivery)
+    ,IF(r.is_pk = 0, 0, IF(amount_edit > 0,1,0)) is_pk
+    ,IF(SUM(IF(r.is_pk  = 0, amount_edit, 0)) OVER (PARTITION BY transaction_id_relink) >3000000,1,0) is_amount_edit_exc_pk_over_3mil
     , l.location
     , l.id location_id
-FROM final f
-LEFT JOIN dwh.dim_location l ON f.branch = l.branch
+FROM return_detail r
+LEFT JOIN dwh.dim_location l ON r.branch = l.branch
+
+)
+,
+
+order_w_pk AS (
+SELECT
+transaction_id_relink,
+CASE WHEN COUNTIF(is_pk = 1) = COUNT(sku) THEN 1 ELSE 0 END order_w_pk
+FROM pk_raw p
+GROUP BY 1
+)
+
+SELECT 
+p.*
+, CASE WHEN o.order_w_pk = 1 THEN p.is_pk ELSE 0 END pk_in_order
+, CASE WHEN p.is_pk = 1 AND p.cat1_pk_id IS NULL THEN 'others' ELSE dp.cat1 END cat1_pk
+FROM pk_raw p
+LEFT JOIN order_w_pk o ON p.transaction_id_relink = o.transaction_id_relink
+LEFT JOIN dwh.dim_product dp ON p.cat1_pk_id = dp.cat1_pk_id 
+                            AND p.sku = dp.sku
+
